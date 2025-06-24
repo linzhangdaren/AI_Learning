0237fd95d3fae0934958a614bc35d187f295add1
linzhangdaren <739088568@qq.com>
2025-06-24 20:25:40 +0800

    使用Numpy转换图像为数组再用plt查看&图像原理&单通道的使用

---
+++ PythonCV/3.2.图像的本质 copy.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+img = Image.open(r'D:\Cache\GitHub_hc\AI_Learning\PythonCV\img\color_circle.png')
+img.show()#会直接弹窗一个图片窗口
