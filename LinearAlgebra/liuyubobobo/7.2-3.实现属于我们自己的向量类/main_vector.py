939aac9d154aec420e010c47148246e578554cf4
linzhangdaren <739088568@qq.com>
2025-06-27 08:39:48 +0800

    类的en,index对象方法的实现

---
+++ LinearAlgebra/liuyubobobo/7.2-3.实现属于我们自己的向量类/main_vector.py
@@ -0,0 +1,10 @@
+#加载playLA里的Vector类
+from playLA.Vector import Vector
+
+if __name__ == '__main__':
+    vec=Vector([1,2,3])
+    print(vec)
+    #vec的len长度
+    print(len(vec))
+    #vec元素索引
+    print(vec[0])
--- LinearAlgebra/liuyubobobo/7.2-3.实现属于我们自己的向量类/playLA/Vector.py
+++ LinearAlgebra/liuyubobobo/7.2-3.实现属于我们自己的向量类/playLA/Vector.py
@@ -2,6 +2,14 @@ class Vector:
     def __init__(self,lst):
         self.values=lst
         
+    #取元素索引
+    def __getitem__(self,index):
+        return self.values[index]
+    
+    #长度len
+    def __len__(self):
+        return len(self.values)
+        
     def __repr__(self):#官方详细->Vector([1,2,3])
         return f"Vector({self.values})"
     def __str__(self):#非正式用户调用:print(对象)->(1, 2, 3)
