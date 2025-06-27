3df395dc81abbe68b9af7f2b7d52dd6729c063bd
linzhangdaren <739088568@qq.com>
2025-06-27 12:04:02 +0800

    实现向量类的基本运算，包括加法、减法、乘法和索引功能，并完善字符串表示方法

--- LinearAlgebra/liuyubobobo/7.2-3.实现属于我们自己的向量类/playLA/Vector.py
+++ LinearAlgebra/liuyubobobo/7.2-3.实现属于我们自己的向量类/playLA/Vector.py
@@ -12,5 +12,5 @@ class Vector:
         
     def __repr__(self):#官方详细->Vector([1,2,3])
         return f"Vector({self.values})"
-    def __str__(self):#非正式用户调用:print(对象)->(1, 2, 3)
-        return f"({', '.join(str(i) for i in self.values)})"
+    def __str__(self):#非正式用户调用:print(对象)->(1,2,3)
+        return f"({','.join(str(i) for i in self.values)})"
---
+++ LinearAlgebra/liuyubobobo/9.2-5实现向量的基本运算/main_vector.py
@@ -0,0 +1,15 @@
+#加载playLA里的Vector类
+from playLA.Vector import Vector
+
+if __name__ == '__main__':
+    vec=Vector([1,2,3])
+    print(vec)
+    #vec的len长度
+    print(len(vec))
+    #vec元素索引
+    print(vec[0])
+    
+    #向量相加
+    vec2=Vector([4,5,6])
+    vec3=vec+vec2
+    print(f"{vec} + {vec2} = {vec3}")
